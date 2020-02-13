62fdc3bf2fd937b70b4814b4847e7552fbdddf6c
ZehaoChen <a15zehcheche@iam.cat>
2020-02-13 16:20:45 +0100

    calendar layput

--- ErrorAtryApp/components/calendar.js
+++ ErrorAtryApp/components/calendar.js
@@ -6,33 +6,39 @@ export default class Calendar extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <View style={day}>
-                    <Text>1</Text>
-                </View>
-                <View style={day}>
-                    <Text>2</Text>
-                </View>
-                <View style={day}>
-                    <Text>3</Text>
+                <View style={styles.dayContainer}>
+                    <View style={styles.day}>
+                        <Text>1</Text>
+                    </View>
+                    <View style={styles.day}>
+                        <Text>2</Text>
+                    </View>
+                    <View style={styles.day}>
+                        <Text>3</Text>
 
-                </View>
-                <View style={day}>
-                    <Text>4</Text>
+                    </View>
+                    <View style={styles.day}>
+                        <Text>4</Text>
 
-                </View>
-                <View style={day}>
-                    <Text>5</Text>
+                    </View>
+                    <View style={styles.day}>
+                        <Text>5</Text>
 
-                </View>
-                <View style={day}>
-                    <Text>6</Text>
+                    </View>
+                    <View style={styles.day}>
+                        <Text>6</Text>
 
-                </View>
-                <View style={day}>
-                    <Text>7</Text>
+                    </View>
+                    <View style={styles.day}>
+                        <Text>7</Text>
 
+                    </View>
                 </View>
-                <View style={calendar}>calendar</View>
+                <View style={styles.calendar}>
+                    <Text>
+                        calendar
+                    </Text>
+                </View>
             </View>
         );
 
@@ -41,13 +47,31 @@ export default class Calendar extends Component {
 }
 const styles = StyleSheet.create({
     container: {
-
+        flex: '1',
+        flexDirection: 'row',
+        backgroundColor: '#ccc',
+        height: 50,
+        alignItems: 'center',
+    },
+    dayContainer:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
     },
     day: {
-
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 38,
+        width: 38,
+        marginHorizontal: 2,
+        backgroundColor: 'red',
+        borderRadius: 10
     },
     calendar: {
-
+        flex:1,
+        height: 50,
+        
+        backgroundColor: '#435',
+        marginHorizontal: 2,
     },
 
 });
